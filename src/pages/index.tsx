0d8c42b6b3e87d5db88a2de32028f0261da6d250
Artur Schincariol Rossi <artursrossi4@gmail.com>
2023-03-17 12:14:58 -0300

    Functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import {
   NumberZero,
@@ -21,7 +22,78 @@ import {
 } from "phosphor-react";
 
 export default function Home() {
-  const ItemSize = 40;
+  const [result, setResult] = useState("0");
+  const [current, setCurrent] = useState("");
+  const ItemSize = 32;
+
+  function clear() {
+    setCurrent("");
+    setResult("0");
+  }
+
+  function backSpace() {
+    setResult((res) => (res.length > 1 ? res.slice(0, -1) : "0"));
+  }
+
+  function addNumber(number: string) {
+    if (number == "0") {
+      setResult((res) => (+res == 0 ? "0" : res + number));
+    } else {
+      setResult((res) => (+res == 0 ? number : res + number));
+    }
+  }
+
+  function plus() {}
+
+  // Começa a dar errado
+  function addToCurrent(type: string) {
+    if (current.length > 1) {
+      calculate();
+    } else {
+      setCurrent(`${result} ${type}`);
+      setResult("0");
+    }
+  }
+
+  function calculate() {
+    let calcResult: number = 0;
+
+    const currentArr = current.split(" ");
+    const numberInCurrent = currentArr[0];
+    const type = currentArr.pop();
+
+    if (type == "=") {
+      clear();
+    }
+
+    if (type == "+") {
+      calcResult = +numberInCurrent + +result;
+      showResult(calcResult);
+    }
+
+    if (type == "-") {
+      calcResult = +numberInCurrent - +result;
+      showResult(calcResult);
+    }
+
+    if (type == "x") {
+      calcResult = +numberInCurrent * +result;
+      showResult(calcResult);
+    }
+
+    if (type == "/") {
+      calcResult = +numberInCurrent / +result;
+      showResult(calcResult);
+    }
+
+    function showResult(number: number) {
+      const fixedResult = parseFloat(number.toFixed(2));
+      const calcResultString = fixedResult.toString().replace(".", ",");
+
+      setCurrent("");
+      setResult(calcResultString);
+    }
+  }
 
   return (
     <>
@@ -36,62 +108,62 @@ export default function Home() {
       </Head>
       <main className="main">
         <div className="display">
-          <div className="current">64.5</div>
-          <div className="result">64.5</div>
+          <div className="current">{current}</div>
+          <div className="result">{result}</div>
         </div>
         <div className="controls">
-          <button className="item">
+          <button onClick={clear} className="item">
             <Eraser size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addToCurrent("%")} className="item">
             <Percent size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addToCurrent("/")} className="item">
             <Divide size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addToCurrent("x")} className="item">
             <X size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("7")} className="item">
             <NumberSeven size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("8")} className="item">
             <NumberEight size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("9")} className="item">
             <NumberNine size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addToCurrent("-")} className="item">
             <Minus size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("4")} className="item">
             <NumberFour size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("5")} className="item">
             <NumberFive size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("6")} className="item">
             <NumberSix size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addToCurrent("+")} className="item">
             <Plus size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("1")} className="item">
             <NumberOne size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("2")} className="item">
             <NumberTwo size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("3")} className="item">
             <NumberThree size={ItemSize} />
           </button>
-          <button id="Equals" className="item">
+          <button id="Equals" onClick={calculate} className="item">
             <Equals size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={backSpace} className="item">
             <Backspace size={ItemSize} />
           </button>
-          <button className="item">
+          <button onClick={() => addNumber("0")} className="item">
             <NumberZero size={ItemSize} />
           </button>
           <button className="item">.</button>
